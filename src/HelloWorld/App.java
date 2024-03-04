9c7d8c3556fe3481d9306981b7b3ea2134958805
Radhi Rasho <Radhi.H@outlook.com>
2024-03-04 16:01:23 +0000

    initial commit

---
+++ src/HelloWorld/App.java
@@ -0,0 +1,10 @@
+package HelloWorld;
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        HelloWorld hello = new HelloWorld();
+
+        System.out.println(hello.GetHello());
+    }
+}
---
+++ src/HelloWorld/HelloWorld.java
@@ -0,0 +1,14 @@
+package HelloWorld;
+
+public class HelloWorld {
+    private String hello;
+
+    public HelloWorld() {
+        this.hello = "Hello World";
+    }
+
+    public String GetHello() {
+        return this.hello;
+    }
+
+}
